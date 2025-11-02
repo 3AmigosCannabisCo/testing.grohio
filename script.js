6691bcfb498a22de018bd402a5aea5bd28636113
3 Amigos Cannabis Co. <ronnywilson073@gmail.com>
2025-11-02 07:50:28 -0500

    Update script.js
    add firebase

--- script.js
+++ script.js
@@ -628,4 +628,24 @@ function debounce(func, timeout = 1000) {
         timer = setTimeout(() => { func.apply(this, args); }, timeout);
     };
 }
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCohZppMXwTkCUxq-bpcKtOwhQmhiZvW34",
+  authDomain: "grohio-3amigos.firebaseapp.com",
+  projectId: "grohio-3amigos",
+  storageBucket: "grohio-3amigos.firebasestorage.app",
+  messagingSenderId: "985498353759",
+  appId: "1:985498353759:web:954433dce5869ab60fdbea",
+  measurementId: "G-N6R2H9PWN6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
